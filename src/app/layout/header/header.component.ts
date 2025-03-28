bc01f46289ebcd879f9d1e2bc0328e945c4e635d
m0nicaRV <156894487+m0nicaRV@users.noreply.github.com>
2025-03-28 08:43:55 +0000

    new file:   .editorconfig     new file:   .gitignore

---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import  {CommonModule} from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'app-header',
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
